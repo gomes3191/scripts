c411767b948d3f48a7abcad1f80a90f54f9a5acb
Francisco Aparecido <gomes.tisystem@gmail.com>
2022-09-04 04:11:27 -0300

    Atualização

--- install_essencial.sh
+++ install_essencial.sh
@@ -1,3 +1,3 @@
-sudo apt-get install rar unrar p7zip p7zip-full p7zip-rar lzma lzma-dev p7zip-full gdebi ttf-mscorefonts-installer vlc gnome-usage ttf-mscorefonts-installer fonts-crosextra-caladea fonts-crosextra-carlito fonts-croscore ubuntu-restricted-extras x264 ffmpeg adobe-flashplugin chromium-codecs-ffmpeg chromium-codecs-ffmpeg-extra xdg-desktop-portal-gtk -y
+sudo apt-get install rar unrar p7zip p7zip-full p7zip-rar lzma lzma-dev p7zip-full gdebi ttf-mscorefonts-installer vlc gnome-usage ttf-mscorefonts-installer fonts-crosextra-caladea fonts-crosextra-carlito fonts-croscore ubuntu-restricted-extras x264 ffmpeg chromium-codecs-ffmpeg-extra xdg-desktop-portal-gtk -y
 
 #libreoffice libreoffice-l10n-pt-br libreoffice-help-pt-br libreoffice-lightproof-pt-br libreoffice-templates libreoffice-style-elementary libreoffice-style-sifr
